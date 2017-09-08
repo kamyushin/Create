971167907847908d128539a97e0b3335f1a2db4a
Kamyu <kamyushin@gmail.com>
2017-09-08 19:02:35 +0900

    add new file

---
+++ Shiamda_Yugo/Assets/Scripts/CurveBullet.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CurveBullet : MonoBehaviour {
+
+    public int speed = 10;
+
+    private float alpha = 1.2f;
+
+    public float lifetime = 5;
+    // Use this for initialization
+
+    public int power = 1;
+
+    private GameObject Player;
+
+    private Rigidbody2D r;
+
+    private Vector2 startV;
+
+    private bool zero = false;
+    void Start()
+    {
+        r = GetComponent<Rigidbody2D>();
+        Player = GameObject.Find("Player(Clone)");
+        r.velocity = transform.up.normalized * speed;
+        startV = r.velocity;
+        Destroy(gameObject, lifetime);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Player != null)
+        {
+            if (zero == false)
+                r.AddForce(-transform.up.normalized * alpha);
+            if (Mathf.Abs(r.velocity.x) <= Mathf.Abs(startV.x * 0.1f) && Mathf.Abs(r.velocity.y) <= Mathf.Abs(startV.y * 0.1f))
+            {
+                zero = true;
+                float rotation = GetAim(transform.position, Player.transform.position);
+                transform.rotation = Quaternion.Euler(0, 0, -90 + rotation);
+
+                r.velocity = transform.up.normalized * speed;
+            }
+        }
+        
+
+    }
+
+    public float GetAim(Vector2 p1, Vector2 p2)
+    {
+        float dx = p2.x - p1.x;
+        float dy = p2.y - p1.y;
+        float rad = Mathf.Atan2(dy, dx);
+        return rad * Mathf.Rad2Deg;
+    }
+}
---
+++ Shiamda_Yugo/Assets/Scripts/Score.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+
+public class Score : MonoBehaviour
+{
+    public GUIText scoreGUIText;
+    
+    public GUIText highScoreGUIText;
+    
+    private int score;
+    
+    public static int highScore;
+    
+    private string highScoreKey = "highScore";
+
+    private static int game_count = 0;
+
+    void Start()
+    {
+        if (game_count == 0)
+        {
+            PlayerPrefs.DeleteAll();
+            game_count++;
+        }
+        
+        Initialize();
+    }
+
+    void Update()
+    {
+        if (highScore < score)
+        {
+            highScore = score;
+        }
+
+        scoreGUIText.text = score.ToString();
+        highScoreGUIText.text = "HighScore : " + highScore.ToString();
+    }
+    
+
+    private void Initialize()
+    {
+        score = 0;
+        
+
+        highScore = PlayerPrefs.GetInt(highScoreKey, 0);
+    }
+    
+    public void AddPoint(int point)
+    {
+        score = score + point;
+    }
+    
+    public void Save()
+    {
+        PlayerPrefs.SetInt(highScoreKey, highScore);
+        PlayerPrefs.Save();
+        
+        Initialize();
+    }
+
+    public void ScoreDown()
+    {
+        score -= 500;
+        if (score <= 0)
+            score = 0;
+    }
+}
